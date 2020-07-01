6f22e41b431999178aa16e43f925fe580b4c80e7
blockiv <blockiv@protonmail.com>
2020-07-01 07:52:00 -0400

    dmenu lines

--- config.def.h
+++ config.def.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-m", dmenumon, "-fn", dmenufont, "-nb", col_base, "-nf", col_ltgray, "-sb", col_red, "-sf", col_black, "-nhb", col_base, "-nhf", col_red, "-shb", col_red, "-shf", col_ltgray, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c","-l", "20", "-m", dmenumon, "-fn", dmenufont, "-nb", col_base, "-nf", col_ltgray, "-sb", col_red, "-sf", col_black, "-nhb", col_base, "-nhf", col_red, "-shb", col_red, "-shf", col_ltgray, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "Scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
