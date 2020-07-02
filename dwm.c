0353407c997c7246ceb561c664c85c6ea8af7156
blockiv <blockiv@protonmail.com>
2020-07-02 03:47:59 -0400

    Fixed typo in dwm.c

--- dwm.c
+++ dwm.c
@@ -717,7 +717,7 @@ dirtomon(int dir)
 void
 drawbar(Monitor *m)
 {
-	int x, w, tw = 0, n - 0, scm;
+	int x, w, tw = 0, n = 0, scm;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
